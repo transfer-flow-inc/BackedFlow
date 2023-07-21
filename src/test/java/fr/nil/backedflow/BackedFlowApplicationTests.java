d0768f49156b534b5dd551d8e9c7ad028ff3dcfd
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-07-21 14:03:43 +0200

    First commit with skeleton of the project

---
+++ src/test/java/fr/nil/backedflow/BackedFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nil.backedflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackedFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
