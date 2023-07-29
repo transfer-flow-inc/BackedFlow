03d5a261057afaefc94732194e772c7da6773ea2
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-07-29 17:50:25 +0200

    Created the folder controller with a get to the id of the folder that returns the folder ( need to add the accessKey to this), a download/folderURL that downloads all the files from the folder by its folderURL requires an access key to download due to the lack of JWT token in this Controller

--- src/main/java/fr/nil/backedflow/controllers/FolderController.java
+++ src/main/java/fr/nil/backedflow/controllers/FolderController.java
@@ -1,16 +1,29 @@
 package fr.nil.backedflow.controllers;
 
+import fr.nil.backedflow.entities.FileEntity;
 import fr.nil.backedflow.entities.Folder;
+import fr.nil.backedflow.exceptions.AccessKeyException;
 import fr.nil.backedflow.exceptions.FolderNotFoundException;
 import fr.nil.backedflow.repositories.FolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,11 +45,37 @@ public class FolderController {
         return ResponseEntity.ok(folder);
     }
 
+    // Define a method to download files
+    @GetMapping("/download/{folderURL}")
+    public ResponseEntity<?> downloadFiles(@PathVariable("folderURL") String folderURL, @RequestParam("accessKey") String accessKey) throws IOException, MalformedURLException, AccessKeyException {
+        if(accessKey.isEmpty())
+            return new ResponseEntity<>("Invalid access key!", HttpStatus.FORBIDDEN);
+
+        Folder folder = folderRepository.getFolderByUrl(folderURL).get();
+
+        if(!accessKey.equals(folder.getAccessKey()))
+            return new ResponseEntity<>("Invalid access key!", HttpStatus.FORBIDDEN);
+
+
+        for(FileEntity file : folder.getFileEntityList()) {
+            Resource resource = new UrlResource("file:" + file.getFilePath());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("File-Name", file.getFileName());
+            httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_VALUE))
+                    .headers(httpHeaders).body(resource);
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(Path.of(folder.getFileEntityList().get(1).getFilePath()))))
+                .body(new UrlResource(folder.getFileEntityList().get(1).getFileName()));
+    }
+
+
+
     @Profile("testing")
     @RestController
     @RequiredArgsConstructor
     @RequestMapping("/api/v1/test/")
-    public class RandomEndpointController {
+    public static class RandomEndpointController {
 
         @Autowired
         private FolderRepository folderRepository;
