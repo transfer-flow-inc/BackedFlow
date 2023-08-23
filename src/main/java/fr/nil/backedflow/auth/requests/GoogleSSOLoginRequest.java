bde27560994c1f38b19dc4b652becb5ec9e3ff83
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-08-23 21:21:54 +0200

    Thanks Google to change the JSON every damn day of the year
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

--- src/main/java/fr/nil/backedflow/auth/requests/GoogleSSOLoginRequest.java
+++ src/main/java/fr/nil/backedflow/auth/requests/GoogleSSOLoginRequest.java
@@ -18,8 +18,62 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GoogleSSOLoginRequest {
 
-    @JsonProperty("info")
-    private GoogleSSOInfoLoginRequest info;
+    /**
+     * The user's Google ID, which is a unique identifier that Google assigns to each user.
+     */
+    @JsonProperty("sub")
+    private String sub;
 
+    /**
+     * The user's email address.
+     */
+    @JsonProperty("email")
+    private String email;
+
+    /**
+     * The user's full name.
+     */
+    @JsonProperty("name")
+    private String name;
+
+    /**
+     * The "authorized party" of the token, which identifies the client that requested the token.
+     * This value corresponds to the client ID of the Google API Console project that was used to
+     * create the OAuth 2.0 client ID for your Angular application.
+     */
+    @JsonProperty("azp")
+    private String azp;
+
+    /**
+     * The "audience" of the token, which specifies the intended recipient of the token.
+     */
+    @JsonProperty("aud")
+
+    private String aud;
+
+    /**
+     * A unique identifier for the token.
+     */
+    @JsonProperty("jti")
+
+    private String jti;
+
+    /**
+     * Gets the first name of the user based on the "name" field.
+     *
+     * @return The first name of the user.
+     */
+    public String getFirstName() {
+        return this.name.split(" ")[0];
+    }
+
+    /**
+     * Gets the last name of the user based on the "name" field.
+     *
+     * @return The last name of the user.
+     */
+    public String getLastName() {
+        return this.name.split(" ")[1];
+    }
 
 }
