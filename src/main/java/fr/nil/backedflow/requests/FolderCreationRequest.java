dfe70cdecf1ba91c79bf0fb6a1a69af996a41255
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-08-25 08:50:33 +0200

    Create a DTO FolderCreationRequest.java
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

---
+++ src/main/java/fr/nil/backedflow/requests/FolderCreationRequest.java
@@ -0,0 +1,20 @@
+package fr.nil.backedflow.requests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FolderCreationRequest {
+
+    private String folderName;
+    private List<String> recipientsEmails;
+    private String message;
+}
