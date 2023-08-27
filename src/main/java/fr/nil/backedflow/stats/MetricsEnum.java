e9dd26d7e929cc7f7e59cde57cc4bf13e704057a
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-08-27 13:55:59 +0200

    Create a MetricsEnum to store all the metrics name.
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

---
+++ src/main/java/fr/nil/backedflow/stats/MetricsEnum.java
@@ -0,0 +1,29 @@
+package fr.nil.backedflow.stats;
+
+
+public enum MetricsEnum {
+
+    USER_CREATION_COUNT("backedflow.user.register.count"),
+    USER_LOGIN_COUNT("backedflow.user.login.count"),
+    USER_SSO_LOGIN_COUNT("backedflow.user.login.sso.count"),
+    USER_VERIFICATION_COUNT("backedflow.user.verification.count"),
+
+
+    FILE_TRANSFER_UPLOAD_COUNT("backedflow.transfer.file.count"),
+    FILE_TRANSFER_UPLOAD_SIZE("backedflow.transfer.upload.file.size"),
+    FILE_TRANSFER_DOWNLOAD_COUNT("backedflow.transfer.download.file.count"),
+    FILE_TRANSFER_DOWNLOAD_SIZE("backedflow.transfer.download.file.sum"),
+
+    ERROR_HANDLER_COUNT("backedflow.error.handler.count");
+
+    private String metricName;
+
+
+    MetricsEnum(String s) {
+        this.metricName = s;
+    }
+
+    public String getMetricName() {
+        return metricName;
+    }
+}
