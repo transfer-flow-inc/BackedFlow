9df2c8b9a7be16a36132f12d2edb76f64e58b0d4
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-08-27 13:59:25 +0200

    Added a meter for the numbers of error caught by the GlobalExceptionHandler
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

--- src/main/java/fr/nil/backedflow/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/fr/nil/backedflow/exceptions/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package fr.nil.backedflow.exceptions.handler;
 
+import fr.nil.backedflow.stats.MetricsEnum;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,14 +10,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@RequiredArgsConstructor
 @ControllerAdvice
 public class GlobalExceptionHandler {
-private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private final MeterRegistry meterRegistry;
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> exception(Exception exception) {
         logger.error("An error has occurred during a HTTP request : " + exception.getMessage());
         logger.debug("Exception happened during a HTTP request : ");
         exception.printStackTrace();
+        meterRegistry.counter(MetricsEnum.ERROR_HANDLER_COUNT.getMetricName()).increment();
         return new ResponseEntity<>("Well that's cringe, something wrong happened try refresh or contact admin@transfer-flow.com", HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
