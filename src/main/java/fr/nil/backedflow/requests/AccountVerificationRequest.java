fc16ea3894d14c1c8b44ca40503a5fe8e021b70c
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-08-30 20:12:50 +0200

    Changed the get to a post created a dto for the AccountVerificationRequest
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

---
+++ src/main/java/fr/nil/backedflow/requests/AccountVerificationRequest.java
@@ -0,0 +1,16 @@
+package fr.nil.backedflow.requests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountVerificationRequest {
+
+    private String token;
+}
