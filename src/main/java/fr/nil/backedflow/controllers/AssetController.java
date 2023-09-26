40940ef2da519b7cfc6105907ce4a715201337d9
Nil MALHOMME <31489080+nil-malh@users.noreply.github.com>
2023-09-26 19:34:05 +0200

    Added all assets to the asset endpoint (#109)
    Signed-off-by: Nil MALHOMME <malhomme.nil+github@gmail.com>

--- src/main/java/fr/nil/backedflow/controllers/AssetController.java
+++ src/main/java/fr/nil/backedflow/controllers/AssetController.java
@@ -18,11 +18,38 @@ import java.io.IOException;
 public class AssetController {
 
     @GetMapping(
-            value = "/logo.png",
+            value = "/logo_dark.png",
             produces = MediaType.IMAGE_PNG_VALUE
     )
     public @ResponseBody byte[] getLogoImage() throws IOException {
         ClassPathResource imgFile = new ClassPathResource("logo_dark.png");
+        return IOUtils.toByteArray(imgFile.getInputStream());
+    }
+
+    @GetMapping(
+            value = "/logo_light.png",
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public @ResponseBody byte[] getLogoLightImage() throws IOException {
+        ClassPathResource imgFile = new ClassPathResource("logo_light.png");
+        return IOUtils.toByteArray(imgFile.getInputStream());
+    }
+
+    @GetMapping(
+            value = "/logo_with_text_dark.png",
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public @ResponseBody byte[] getLogoTextImage() throws IOException {
+        ClassPathResource imgFile = new ClassPathResource("logo_with_text_dark.png");
+        return IOUtils.toByteArray(imgFile.getInputStream());
+    }
+
+    @GetMapping(
+            value = "/logo_with_text_light.png",
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public @ResponseBody byte[] getLogoTextLightImage() throws IOException {
+        ClassPathResource imgFile = new ClassPathResource("logo_with_text_light.png");
         return IOUtils.toByteArray(imgFile.getInputStream());
     }
 }
